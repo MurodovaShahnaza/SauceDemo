4997180f76af2452c602234aaa5f7a535d99ee97
Pororo <99640905+MurodovaShahnaza@users.noreply.github.com>
2025-04-29 16:46:53 +0300

    Add files via upload

---
+++ SauceDemo/src/test/java/tests/LoginTest.java
@@ -0,0 +1,48 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class LoginTest extends BaseTest{
+
+
+
+
+    @Test
+    public void checkLoginWithEmptyPassword() {
+        loginPage.open();
+        loginPage.login("standard_user", "");
+        assertEquals(loginPage.getErrorMessage(),
+                "Epic sadface: Password is required",
+                "Сообщение об ошибке не появилось");
+    }
+
+    @Test
+    public void checkLoginWithWrongPassword() {
+        loginPage.open();
+        loginPage.login("standard_user", "123456");
+        assertEquals(loginPage.getErrorMessage(),
+                "Epic sadface: Username and password do not match any user in this service",
+                "Сообщение об ошибке не появилось");
+    }
+
+    @Test
+    public void checkLoginWithEmptyUsername() {
+        loginPage.open();
+        loginPage.login("", "secret_sauce");
+        assertEquals(loginPage.getErrorMessage(),
+                "Epic sadface: Username is required",
+                "Сообщение об ошибке не появилось");
+    }
+
+    @Test
+    public void checkLoginWithPositiveCred(){
+        loginPage.open();
+        loginPage.login("standard_user", "secret_sauce");
+        assertEquals(productsPage.getTitle(),
+                "Products",
+                "Логин не выполнен");
+    }
+}
---
+++ SauceDemo/src/test/java/tests/OverviewTest.java
@@ -0,0 +1,5 @@
+package tests;
+
+public class OverviewTest extends BaseTest{
+
+}
