8774b8febeb282068029c10e5ce122dcb5696535
Pororo <99640905+MurodovaShahnaza@users.noreply.github.com>
2025-04-29 16:14:07 +0300

    Add files via upload

---
+++ SauceDemo/src/test/java/tests/BaseTest.java
@@ -0,0 +1,49 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import pages.CheckoutPage;
+import pages.LoginPage;
+import pages.OverviewPage;
+import pages.ProductsPage;
+
+import java.time.Duration;
+import java.util.HashMap;
+
+public class BaseTest {
+
+    WebDriver driver;
+    LoginPage loginPage;
+    ProductsPage productsPage;
+    CheckoutPage checkoutPage;
+    OverviewPage overviewPage;
+
+    @BeforeMethod
+    public void setup() {
+        ChromeOptions options = new ChromeOptions();
+        HashMap<String, Object> chromePrefs = new HashMap<>();
+        chromePrefs.put("credentials_enable_service", false);
+        chromePrefs.put("profile.password_manager_enabled", false);
+        options.setExperimentalOption("prefs", chromePrefs);
+        options.addArguments("--incognito");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-popup-blocking");
+        options.addArguments("--disable-infobars");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        loginPage = new LoginPage(driver);
+        productsPage = new ProductsPage(driver);
+        checkoutPage = new CheckoutPage(driver);
+        overviewPage = new OverviewPage(driver);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
---
+++ SauceDemo/src/test/java/tests/CheckoutTest.java
@@ -0,0 +1,88 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CheckoutTest extends BaseTest {
+
+
+    @Test
+    public void CheckEmptyFieldsError() {
+
+        loginPage.open();
+        loginPage.login("standard_user","secret_sauce");
+        productsPage.getTitle();
+        productsPage.ClickAdd();
+        productsPage.CheckProductInTheCart();
+        productsPage.Checkout();
+        checkoutPage.getTitle();
+        checkoutPage.check("", "", "");
+        Assert.assertEquals(checkoutPage.getErrorMessage(),
+                "Error: First Name is required",
+                "Сообщение об ошибке не появилось");
+    }
+
+    @Test
+    public void CheckTheFirstNameError() {
+
+        loginPage.open();
+        loginPage.login("standard_user","secret_sauce");
+        productsPage.getTitle();
+        productsPage.ClickAdd();
+        productsPage.CheckProductInTheCart();
+        productsPage.Checkout();
+        checkoutPage.getTitle();
+        checkoutPage.check("", "TestTest", "123456789");
+        Assert.assertEquals(checkoutPage.getErrorMessage(),
+                "Error: First Name is required",
+                "Сообщение об ошибке не появилось");
+    }
+
+    @Test
+    public void CheckTheLastNameError() {
+
+        loginPage.open();
+        loginPage.login("standard_user","secret_sauce");
+        productsPage.getTitle();
+        productsPage.ClickAdd();
+        productsPage.CheckProductInTheCart();
+        productsPage.Checkout();
+        checkoutPage.getTitle();
+        checkoutPage.check("Test", "", "123456789");
+        Assert.assertEquals(checkoutPage.getErrorMessage(),
+                "Error: Last Name is required",
+                "Сообщение об ошибке не появилось");
+    }
+
+    @Test
+    public void CheckThePostalCodeError() {
+
+        loginPage.open();
+        loginPage.login("standard_user","secret_sauce");
+        productsPage.getTitle();
+        productsPage.ClickAdd();
+        productsPage.CheckProductInTheCart();
+        productsPage.Checkout();
+        checkoutPage.getTitle();
+        checkoutPage.check("Test", "TestTest", "");
+        Assert.assertEquals(checkoutPage.getErrorMessage(),
+                "Error: Postal Code is required",
+                "Сообщение об ошибке не появилось");
+    }
+
+    @Test
+    public void CheckCorrectFilling() {
+
+        loginPage.open();
+        loginPage.login("standard_user","secret_sauce");
+        productsPage.getTitle();
+        productsPage.ClickAdd();
+        productsPage.CheckProductInTheCart();
+        productsPage.Checkout();
+        checkoutPage.getTitle();
+        checkoutPage.check("Test", "TestTest", "123456789");
+        Assert.assertEquals(overviewPage.getTitle(),
+                "Checkout: Overview",
+                "Заказ не совершен");
+    }
+}
