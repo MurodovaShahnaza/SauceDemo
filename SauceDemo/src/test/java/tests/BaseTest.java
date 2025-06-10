20fc7cf5a30990cf50ee93c24aa6217a6e21428f
ps 24 <murodova2002@mail.ru>
2025-06-10 21:40:40 +0300

    added headless

--- SauceDemo/src/test/java/tests/BaseTest.java
+++ SauceDemo/src/test/java/tests/BaseTest.java
@@ -32,6 +32,7 @@ public class BaseTest {
         options.addArguments("--disable-notifications");
         options.addArguments("--disable-popup-blocking");
         options.addArguments("--disable-infobars");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
